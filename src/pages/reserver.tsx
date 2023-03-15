9740486c0f5fb6652b368d35ff0afa3cdc7c7e47
Halias3422 <v.desaintexupery@gmail.com>
2023-03-15 23:07:44 +0100

    feat(reserver): add placeholder text

--- src/pages/reserver.tsx
+++ src/pages/reserver.tsx
@@ -13,7 +13,7 @@ const ReserverPage = ({ weekSchedule }: { weekSchedule: DaySchedule[] }) => {
         <Hero
           header="À quelques clics d'une <b>expérience</b> unique !"
           paragraphs={[
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at ante pharetra nisl suscipit porta. Orci varius natoque penatibus et magnis dis parturient montes.',
+            "Notre équipe est prête à vous accueillir. Remplissez le formulaire et n'hésitez pas à renseigner vos demandes ou contraintes spécifiques. Nous nous occupons de tout, vous n'avez plus qu'à profiter.",
           ]}
           image="service-is-ready.jpg"
           imageAlt="Le restaurant est prêt à vous acceuillir !"
@@ -25,8 +25,8 @@ const ReserverPage = ({ weekSchedule }: { weekSchedule: DaySchedule[] }) => {
           id="howDoesItWorkSection"
           header="Comment ça marche ?"
           paragraphs={[
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at ante pharetra nisl suscipit porta. Orci varius natoque penatibus et magnis dis parturient montes.',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at ante pharetra nisl suscipit porta. Orci varius natoque penatibus et magnis dis parturient montes.',
+            "La demande de réservation est prise en compte une fois le formulaire complété envoyé. Vous n'avez rien d'autre à faire. Vous pouvez également effectuer votre réservation en nous contactant par téléphone au 01.02.03.04.05.",
+            "Pour toute modification ou annulation concernant votre réservation, contactez-nous par téléphone. Nous vous prions de bien vouloir nous prévenir au moins 24h à l'avance afin de garantir le meilleur service.",
           ]}
           image="landline-phone.jpg"
           imageAlt="En cas de questions, appelez nous"
